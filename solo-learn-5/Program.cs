74f2423c2a11da81305a8e541bd0b81f19fc6828
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-21 16:43:17 +0700

    Solo Learn Training

---
+++ solo-learn-5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solo_learn_5
+{
+    internal class Program
+    {
+        class Fields
+        {
+            public int x = 5;
+            public int y = 5;
+            public int z = 3;
+        }
+        
+        public static void Main(string[] args)
+        {
+            Fields f  = new Fields();
+            int x = 6;
+            int y = 9;
+            int z = 7;
+
+            x = f.x;
+            f.y = y;
+            z = f.z;
+            Console.WriteLine(f.x + f.y*z);
+            
+            
+            
+            int value;
+            value = Convert.ToInt32(!("Gari" == "Kai"));
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ solo-learn-6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solo_learn_6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.name = "Gosho";
+            Console.Write(p1.name[2]);
+            Console.Write(p1.country[4]);
+        }
+        class Person
+        {
+            public string name;
+            public string country;
+
+            public Person()
+            {
+                name = "Petar";
+                country = "Bulgaria";
+            }
+        }
+    }
+}
