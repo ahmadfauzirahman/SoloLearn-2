04f662737098eeb09440dee05ec8d5d5ea3c5a21
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-21 16:31:33 +0700

    Solo Learn Training

---
+++ solo-learn-3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solo_learn_3
+{
+    internal class Program
+    {
+        static int inc(int arg)
+        {
+            return arg += 1;
+        }
+
+        static int Fibonaci(int x)
+        {
+            if (x <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonaci(x - 1) + Fibonaci(x - 2);
+            }
+        }
+
+        static int Func(int d = 3, int e = 7, int f = 6)
+        {
+            return d + e * f;
+        }
+        public static void Main(string[] args)
+        {
+
+            int d = 4;
+            int e = 5;
+            int f = 2;
+            Console.WriteLine(Func(f,e));
+            
+            Console.WriteLine("------");
+            Console.WriteLine("------");
+            Console.WriteLine("------");
+            
+            int a = 0;
+            a++;
+            var res = inc(a++);
+            Console.WriteLine(res++);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            int result = Fibonaci(4);
+            Console.WriteLine(result);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string x = "";
+            string y = "SolorLearn";
+            x += y.Substring(4, 1);
+            x += y.Substring(3, 1);
+            x += y.Substring(7, 1);
+            x += y.Substring(5, 1);
+            x += y.Substring(8, 1);
+            Console.Write(x);
+            
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int s = 10;
+            do
+            {
+                s *= 2;
+            } while (s <= 5);
+            Console.WriteLine(s);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            int u = 12;
+            u = u << 2;
+            Console.WriteLine(u);
+
+        }
+    }
+}
---
+++ solo-learn-4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solo_learn_4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double d = 1.0;
+            double[] arr = {3.0, 0.5, 2.0, 1.5};
+            for (int i = 0; i < arr.Length; i++)
+            {
+                d *= arr[i];
+            }
+            Console.WriteLine(d);
+            
+            Console.WriteLine("--------------");
+            int x = 10;
+            do
+            {
+                x *= 2;
+            } while (x <= 5);
+            Console.WriteLine(x);
+            Console.WriteLine("--------------");
+            int s = 1;
+            int[] darr = {9, 4, 5};
+            foreach (int i in darr)
+            {
+                if (i % 2 == 0)
+                {
+                    x /= s;
+                }
+                else
+                {
+                    s *= i;
+                }
+            }
+            Console.WriteLine(s);
+            Console.WriteLine("--------------");
+
+            int k = 0;
+            for (int h = 0; h < 9; h++)
+            {
+                k++;
+                h++;
+            }
+            Console.WriteLine(k);
+            Console.WriteLine("--------------");
+
+            Dictionary<string,int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("wood",1500);
+            dictionary.Add("food",3000);
+            dictionary.Add("iron",4500);
+            if (dictionary.ContainsKey("gold"))
+            {
+                Console.WriteLine(6000);
+            }
+            else
+            {
+                int value = dictionary["iron"];
+                Console.WriteLine(value);
+            }
+            Console.WriteLine("--------------");
+            int[] n = new int[5];
+            int w;
+            for (w = 0; w < 5 ; w++)
+            {
+                Console.Write(n[w]);
+            }
+        }
+    }
+}
