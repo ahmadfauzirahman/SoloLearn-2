c92ad2406bc9f557226333eec3ed8ad6a2215671
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-21 10:32:49 +0700

    Training C#

---
+++ SoloLearn-2/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace SoloLearn_2
+{
+    public class Item
+    {
+        public int Number { get; set; }
+        public Item (int x )
+        {
+            Number = x;
+        }
+
+        public static Item operator +(Item a, Item b)
+        {
+            int c = a.Number - b.Number;
+            Item res = new Item(c);
+            return res;
+        }
+    }
+}
---
+++ SoloLearn-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoloLearn_2
+{
+    internal class Program
+    {
+        static int Sum(int x)
+        {
+            if (x != 0)
+            {
+                return x + Sum(x - 1);
+            }
+            else
+            {
+                return x;
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            int x = 6;
+            x = Sum(x);
+            Console.WriteLine(x);
+            Console.WriteLine(++x);
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            Item n1 = new Item(5);
+            Item n2 = new Item(4);
+            Item n3 = n1 + n2;
+            Console.WriteLine(n3.Number);
+        }
+    }
+}
