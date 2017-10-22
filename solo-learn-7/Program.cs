57e2485548b0256a5f9ca88de6864dd346f6006d
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-22 18:59:48 +0700

    Solo Learn Training

---
+++ solo-learn-7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solo_learn_7
+{
+    internal class Program
+    {
+        static int sm(ref int a, ref double A)
+        {
+            --a;
+            --A;
+            if (a.GetType() == typeof(int))
+                return (int) A;
+            return a;
+        }
+        
+        public static void Main(string[] args)
+        {
+            int A = 5;
+            double a = -1.4;
+            A = sm(ref A, ref a);
+            Console.WriteLine(Math.Abs(Math.Ceiling(A+a)));
+        }
+    }
+}
